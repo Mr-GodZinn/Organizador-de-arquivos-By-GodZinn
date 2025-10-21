28732c80ab0636e90f9da3a00c68a629da613544
GodZinn <164184690+Mr-GodZinn@users.noreply.github.com>
2025-10-20 21:36:59 -0300

    Modify Python paths in organizer.bat
    Updated user-specific paths for Python interpreter and script.

--- organizer.bat
+++ organizer.bat
@@ -1,12 +1,14 @@
 @echo off
-REM Caminho para o Python
-SET PYTHON_PATH=C:\Users\GodZinn Lindu\AppData\Local\Programs\Python\Python314\python.exe
+REM Coloque o seu caminho para o interpretador Python aqui
+SET PYTHON_PATH=C:\Users\Seu Usuario\AppData\Local\Programs\Python\Python314\python.exe
 
-REM Caminho para o seu script Python
-SET SCRIPT_PATH=C:\Users\GodZinn Lindu\Documents\Python\main.py
+REM Altere para o caminho para o seu script Python
+SET SCRIPT_PATH=C:\Users\Seu Usuario\Documents\Python\main.py
 
-REM Executa o script
+REM 
 "%PYTHON_PATH%" "%SCRIPT_PATH%"
 
-REM Espera o usuário pressionar uma tecla antes de fechar
+REM 
 pause
+
+
